ddc5c848c0dd477bc4a845612b5818317800d6ec
tomba-howard <111522425+tomba-howard@users.noreply.github.com>
2022-12-16 11:19:33 -0500

    Update databases.php

--- legacy_application/src/priv_prod/opt/library/prd/WebServer/Documents/databases.php
+++ legacy_application/src/priv_prod/opt/library/prd/WebServer/Documents/databases.php
@@ -33,7 +33,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 	$file_link[$row['database']] = $row['link'];
 }
 
-$show['photos1'] = "The ID";
+$show['photos1'] = "Hello World 3";
 //echo "<pre>";print_r($show);echo "</pre>";
 //echo "<pre>";print_r($file_link);echo "</pre>";
 
